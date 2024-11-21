5927fecf1b7218f995d132b67282fdad0f725f8e
Dady francisco mendes <dev_dady@outlook.com>
2024-11-20 22:44:26 -0300

    Primeiro commit: adicionando o projeto Spring Boot

---
+++ src/test/java/com/dadytech/testeautomatizado/TesteAutomatizadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dadytech.testeautomatizado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAutomatizadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
