5933c142fa073644dbb378d973de856f1cce4f17
Dady francisco mendes <dev_dady@outlook.com>
2024-11-21 00:05:10 -0300

    Ajustada configuração do Maven para incluir dependências de testes

---
+++ src/test/java/com/dadytech/testeautomatizado/br/com/dadytech/FooBarTest.java
@@ -0,0 +1,14 @@
+package com.dadytech.testeautomatizado.br.com.dadytech;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Fail.fail;
+
+public class FooBarTest {
+    @Test
+    public void test(){
+        //fail("Not jet implenented");
+         System.out.println("dady ");
+    }
+}
