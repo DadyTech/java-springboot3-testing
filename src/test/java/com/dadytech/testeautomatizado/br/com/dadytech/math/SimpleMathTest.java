49c0e148722b3a3a07a3662cd051ce611bd2da40
Dady francisco mendes <dev_dady@outlook.com>
2024-11-24 20:42:54 -0300

    Conhecendo Outras Assertions

--- src/test/java/com/dadytech/testeautomatizado/br/com/dadytech/math/SimpleMathTest.java
+++ src/test/java/com/dadytech/testeautomatizado/br/com/dadytech/math/SimpleMathTest.java
@@ -12,6 +12,8 @@ public class SimpleMathTest {
        Double actual = xa.sum(6.2D,2D);
        double expected = 8.2D;
        Assertions.assertEquals(expected,actual,"6.2 + 2 did not produce expected 8.2!");
+       Assertions.assertNotEquals(9.2,actual);
+       Assertions.assertNotNull(actual);
    }
 
 }
