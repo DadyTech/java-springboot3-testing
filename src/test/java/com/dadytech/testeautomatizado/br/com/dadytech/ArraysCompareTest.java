4f6a35fc5a9fd6e8692a7b1ce408a043e145b567
Dady francisco mendes <dev_dady@outlook.com>
2024-11-26 22:32:10 -0300

    Testando Performance em JUnit Tests

--- src/test/java/com/dadytech/testeautomatizado/br/com/dadytech/ArraysCompareTest.java
+++ src/test/java/com/dadytech/testeautomatizado/br/com/dadytech/ArraysCompareTest.java
@@ -1,8 +1,10 @@
 package com.dadytech.testeautomatizado.br.com.dadytech;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.fail;
@@ -16,5 +18,17 @@ public class ArraysCompareTest {
 
         Arrays.sort(numbers);
         assertArrayEquals(numbers,expectedArray);
+    }
+    @Test
+    //@Timeout(1)
+    @Timeout(value = 15,unit = TimeUnit.MILLISECONDS)
+    void testSortPerfomance(){
+        int[] numbers = {25,8,21,32,3};
+        for (int i = 0;i < 1000; i++){
+             numbers[0] = i;
+             Arrays.sort(numbers);
+        }
+
+
     }
 }
