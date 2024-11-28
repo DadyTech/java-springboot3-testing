c9d1d47895d92317308479878f5fb6ea498d154a
Dady francisco mendes <dev_dady@outlook.com>
2024-11-28 00:12:52 -0300

    @ParameterizedTest com @MethodSource

--- src/test/java/com/dadytech/testeautomatizado/br/com/dadytech/math/SimpleMathTestS3.java
+++ src/test/java/com/dadytech/testeautomatizado/br/com/dadytech/math/SimpleMathTestS3.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class SimpleMathTest {
+public class SimpleMathTestS3 {
 
    // test[System Under Test]_[Condition or State Change]_[Expented Result]
    SimpleMath xa;
